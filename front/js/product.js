f00b66f2b0de9ea4da353aa6288ac104ca531d0e
apralyn@gmail.com <apralyn@gmail.com>
2022-05-01 02:25:26 -0700

    wip

--- front/js/product.js
+++ front/js/product.js
@@ -1,15 +1,38 @@
-//URLSearch Params to get the product ID
+/* Step by Step Guide on product.js page */
+// Step 01 - URLSearch Param
+    // this step is essential so that the code can pull the correct id from the current URL. 
+
+// Step 02 - fetching the project API 
+
+// Step 03 - function that will display the product information
+
+// Step 04 - use eventlisteners to GET the user input data
+
+// Step 05 - addToCart() will check if... 
+
+// Step 06 - create an object cartItem that will hold the actual and correct user input data to use as a key value pair for localStorage
+
+// Step 07 - check localStorage using getItem to see if there's an item there, if not set the Item in the localStorage. 
+    // do i create a function for this? 
+
+// Step 08 - end goal
+    //  successfuly add item in cart, 
+    // store the item in the localStorage, 
+    // update the item values in the local storage when new items are added.
+
+
+// STEP01 - URLSearch Params to get the product ID
 //product url
 const productUrl = window.location.href;
 //URL object to access search params
 const newProductUrl = new URL(productUrl);
 //product ID
 const productId = newProductUrl.searchParams.get('id');
-console.log(productId);
+// console.log(productId);
 // this also has the product ID that I need
 
 
-
+//STEP02 Fetch API + productId
 // I got the rest of the product data from the API using fetch
 fetch ("http://localhost:3000/api/products/" + productId)
   .then ((data) => {
@@ -20,7 +43,7 @@ fetch ("http://localhost:3000/api/products/" + productId)
     console.log(prodName);
     // this has the product ID that I need
     
-
+//STEP03 - displaySingleProduct()
     //populate the item details in this area from the API to show in the product page.
   function displaySingleProduct(product) {
   
@@ -53,14 +76,7 @@ fetch ("http://localhost:3000/api/products/" + productId)
     }
   })
 
-/* ---- next task: user input data will be added to addToCart()  ------
------------------ localStorage() --------------
-*/
-
-// I have the cart
-  const cart = []; // an array to hold the cart items
-//I just want to add one item to the cart. 
-
+//STEP04 - use eventListener
 // I have the product ID
   console.log(productId);
 // I have the color choice
@@ -80,8 +96,7 @@ function userInputEvents () {
 const addToCartButton = document.getElementById('addToCart');
 addToCartButton.addEventListener('click', addToCart); // the addtoCart button event listener will trigger the addToCart function.
 
-/* Goal for the next hour - done */
-/* I need to figure out how i can move the data into a function */
+//STEP05 addToCart()
 
 function addToCart() {
 console.log('Youre cart has ' + productId + ' the color you chose is ' + chooseColor.value + ' and you got ' + addQty.value + ' of em.');
@@ -89,17 +104,26 @@ console.log('Youre cart has ' + productId + ' the color you chose is ' + chooseC
 addToCart();
 
 
-/* next goal */
-/* toLocalStorage function and parameters 
--- this function will...
-  -- 01 check the localStorage using get if there's items present
-  -- 02 if there's no item use set to store item into the localStorage
--- parameters to be aware of...
-  -- localStorage only accepts key value pair which means that i need to put the data in an object.
-*/
+//STEP06 cartItem object
+
 const cartItem = { 
   cartItemID: productId,
   cartItemColor: chooseColor,
   cartItemQty: addQty
 }
 console.log(cartItem); //checking if this actually works
+
+//STEP07 localStorage
+
+window.localStorage.setItem("cart", JSON.stringify(cartItem));
+const jsonString = localStorage.getItem("cart");
+const cartObject = JSON.parse(jsonString);
+
+console.log(jsonString);
+console.log(cartObject);
+
+function toLocalStorage() {
+//check if there's something in the localStorage
+
+}
+toLocalStorage();
