6828a8abf0a1c0625abe2ba7bf8f3a1a324545e8
Apralyn Eribal <apralyn@gmail.com>
2022-10-22 23:45:43 -0700

    cart items card wip 25%

--- front/js/cart.js
+++ front/js/cart.js
@@ -27,30 +27,30 @@ function createEachItemCard() {
   // TODO insert cards that was added in the cart
   //    TODO loop things added to the cart which is an array from the local storage - done
   //    TODO while there are items in the cart the loop will go through each of the items and will insert cart items in the page. I need product information for each cart item from the products variable - see line 2 on TODO. (use innerHTML with back ticks)
-  for (cartItem of cart) {
+  for (let cartItem of cart) {
     // console.log(cartItem);
-    document.getElementById("cart__items").innerHTML = `
-    <article class="cart__item" data-id="${allProductsData._id}" data-color="${allProductsData.color}">
-                <div class="cart__item__img">
-                  <img src=${allProductsData.imgUrl} alt="Photo of a sofa">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${allProductsData.name}</h2>
-                    <p>Green</p>
-                    <p>${allProductsData.price}</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Delete</p>
-                    </div>
-                  </div>
-                </div>
-              </article>
-    `;
+
+    //parent Article
+    let cartItems = document.getElementById("cart__items"); // not sure why document.querySeletor was not working
+    let itemArticle = document.createElement("article");
+    itemArticle.classList.add("cart__item");
+    itemArticle.setAttribute("data-id", `${cartItem.id}`);
+    itemArticle.setAttribute("data-color", `${cartItem.color}`);
+    cartItems.appendChild(itemArticle);
+    
+
+    // Article child no.1 div for cart item img
+    let cartItemImg = document.createElement("div");
+    cartItemImg.classList.add("cart__item__img");
+    itemArticle.appendChild(cartItemImg);
+    let img = document.createElement("img");
+    cartItemImg.appendChild(img);
+    img.setAttribute("src", `${allProductsData.imageUrl}`);
+
+    // Article child no.2 div for cart item content
+    let cartItemContent = document.createElement("div");
+    cartItemContent.classList.add("cart__item__content");
+
+    itemArticle.appendChild(cartItemContent);
   }
 }
