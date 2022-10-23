162757e11f3ac2286ecfc4e0d0580ae23dfd6d6d
Apralyn Eribal <apralyn@gmail.com>
2022-10-22 17:47:06 -0700

    wip creating card with back ticks

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,21 +5,14 @@
 // >>>>>>>> CART <<<<<<<<<<
 const cart = JSON.parse(localStorage.getItem("cart"));
 let allProductsData = [];
-// for (const items of cart) {
-//    console.log("all cart item IDs ", items.id);//all the ids from the items in the cart
-// }
 
 // will check if isCartEmpty or NOT.
 let isCartEmpty = cart === null; // always "null" when localStorage is empty/emptied.
 if (isCartEmpty) {
   //   console.log("the CART inside the local storage is currently empty.");
-  // alert("The cart is empty!");
-} else {
-  // place cart--items here
-  let showCartItems = document.getElementsByClassName("cart__item");
 }
 
-// >>>>>>>> PRODUCTS <<<<<<<<<<
+// >>>>>>>>  All PRODUCTS data  <<<<<<<<<<
 fetch("http://localhost:3000/api/products/")
   .then((data) => {
     return data.json(); // all products data is returned as json format
@@ -27,21 +20,15 @@ fetch("http://localhost:3000/api/products/")
   .then((products) => {
     // "products" contains all the products data from the API
     allProductsData = products;
-    createItemCard();
+    createEachItemCard();
   });
-// merge cart with localstorage to create a full object like allDataInObj (might need to assign to a new array)
 
-// loop through those newly created objects  
-// call the createItemCard
-
-
-function createItemCard() {
+function createEachItemCard() {
   // TODO insert cards that was added in the cart
   //    TODO loop things added to the cart which is an array from the local storage - done
-  //    TODO while there are items in the cart the loop will go through each of the items and will insert cart items in the page. I need product information for each cart item from the products variable - see line 2 on TODO. (use innerHTML with back ticks) 
+  //    TODO while there are items in the cart the loop will go through each of the items and will insert cart items in the page. I need product information for each cart item from the products variable - see line 2 on TODO. (use innerHTML with back ticks)
   for (cartItem of cart) {
     console.log(cartItem);
+
   }
 }
-
-// createItemCard(allDataInObj);
